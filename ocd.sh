ca58eed9c20b4e08e61f52f37c2bd90ca7d833c2
nycksw <nycksw@gmail.com>
2023-09-29 07:06:13 -0600

    2 shellcheck tests I no longer willingly violate.

--- ocd.sh
+++ ocd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#shellcheck disable=SC1091,SC2086,SC2164
+#shellcheck disable=SC2086
 #
 # OCD: Optimally Configured Dotfiles
 # See https://github.com/nycksw/ocd for detailed information.
